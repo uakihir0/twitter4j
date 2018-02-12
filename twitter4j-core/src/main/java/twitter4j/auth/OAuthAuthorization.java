db29afb1067ce552aed3cfa051f467339f7d1a81
Akihiro Urushihara <a.urusihara@gmail.com>
2018-02-12 17:52:13 +0900

    HmacSHA1 -> SHA1

--- twitter4j-core/src/main/java/twitter4j/auth/OAuthAuthorization.java
+++ twitter4j-core/src/main/java/twitter4j/auth/OAuthAuthorization.java
@@ -319,7 +319,30 @@ public class OAuthAuthorization implements Authorization, java.io.Serializable, 
             throw new AssertionError(ike);
         } catch (NoSuchAlgorithmException nsae) {
             logger.error("Failed to get HmacSHA1 \"Message Authentication Code\" (MAC)", nsae);
-            throw new AssertionError(nsae);
+
+            try {
+                Mac mac = Mac.getInstance("SHA1");
+                SecretKeySpec spec;
+                if (null == token) {
+                    String oauthSignature = HttpParameter.encode(consumerSecret) + "&";
+                    spec = new SecretKeySpec(oauthSignature.getBytes(), "SHA1");
+                } else {
+                    spec = token.getSecretKeySpec();
+                    if (null == spec) {
+                        String oauthSignature = HttpParameter.encode(consumerSecret) + "&" + HttpParameter.encode(token.getTokenSecret());
+                        spec = new SecretKeySpec(oauthSignature.getBytes(), "SHA1");
+                        token.setSecretKeySpec(spec);
+                    }
+                }
+                mac.init(spec);
+                byteHMAC = mac.doFinal(data.getBytes());
+            } catch (InvalidKeyException ike) {
+                logger.error("Failed initialize \"Message Authentication Code\" (MAC)", ike);
+                throw new AssertionError(ike);
+            } catch (NoSuchAlgorithmException nsaee) {
+                logger.error("Failed to get HmacSHA1 \"Message Authentication Code\" (MAC)", nsae);
+                throw new AssertionError(nsae);
+            }
         }
         return BASE64Encoder.encode(byteHMAC);
     }
